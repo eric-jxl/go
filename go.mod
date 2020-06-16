53cebc433ac9fb356c35e1b89b7d98c92140af24
eric <jiangxiaoliang@rhcpa.com>
2020-06-16 18:07:02 +0800

    modify doc and go file

---
+++ go.mod
@@ -0,0 +1,5 @@
+module awesomeProject
+
+go 1.13
+
+require fyne.io/fyne v1.3.0
---
+++ gowin.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+)
+
+func main() {
+	app := app.New()
+
+	w := app.NewWindow("Hello")
+	w.SetContent(widget.NewVBox(
+		widget.NewLabel("Hello Eric!"),
+		widget.NewButton("Quit", func() {
+			app.Quit()
+		}),
+	))
+
+	w.ShowAndRun()
+}
+
