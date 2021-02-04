78a6f3fd10e879c871c14808fbc10bd369dd59be
eric <jiangxiaoliang@rhcpa.com>
2021-02-04 11:25:31 +0800

    修改

--- test/test.go
+++ test/test.go
@@ -19,24 +19,24 @@ func main()  {
 	//if err != nil  {
 	//	fmt.Println(err)
 	//}
-	//const (
-	//	x = iota // x == 0
-	//	y = iota // y == 1
-	//	z = iota // z == 2
-	//	w
-	//)
-	//fmt.Println( x, y, z,w)
-	//var arr [10]int  // 声明了一个int类型的数组
-	//arr[0] = 42      // 数组下标是从0开始的
-	//arr[1] = 13      // 赋值操作
-	//c := [...]int{4, 5, 6}
-	//fmt.Printf("The first element is %d\n", arr[0])  // 获取数据，返回42
-	//fmt.Printf("The last element is %d\n", arr[9])
-	//fmt.Println(c)
-	//slice := []byte {'a', 'b', 'c', 'd'}
-	//fmt.Println(len(slice))
+	const (
+		x = iota // x == 0
+		y = iota // y == 1
+		z = iota // z == 2
+		w
+	)
+	fmt.Println( x, y, z,w)
+	var arr [10]int  // 声明了一个int类型的数组
+	arr[0] = 42      // 数组下标是从0开始的
+	arr[1] = 13      // 赋值操作
+	c := [...]int{4, 5, 6}
+	fmt.Printf("The first element is %d\n", arr[0])  // 获取数据，返回42
+	fmt.Printf("The last element is %d\n", arr[9])
+	fmt.Println(c)
+	slice := []byte {'a', 'b', 'c', 'd'}
+	fmt.Println(len(slice))
 
-	// 假设用户名 abc，密码 123456
+	//假设用户名 abc，密码 123456
 	h := md5.New()
 	var pwd string
 	fmt.Println("输入密码:")
