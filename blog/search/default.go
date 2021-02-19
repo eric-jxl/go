6bc0402f43b6977402d0709494917b01e34fc382
eric <jiangxiaoliang@rhcpa.com>
2021-02-19 10:55:39 +0800

    edit code error

--- blog/search/default.go
+++ blog/search/default.go
@@ -6,6 +6,6 @@ func init() {
 	var matcher defaultMacher
 	Register("default", matcher)
 }
-func (m defaultMatcher) Search(feed *Feed, searchTerm string) ([]*Result, error) {
+func (m defaultMacher) Search(feed *Feed, searchTerm string) ([]*Result, error) {
 	return nil, nil
 }
