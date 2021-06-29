872c635aebf2094678aaddb13f1b604edec9a926
eric <jiangxiaoliang@rhcpa.com>
2021-06-29 09:40:50 +0800

    add new gin web file
    Signed-off-by: eric <jiangxiaoliang@rhcpa.com>

--- test/test.go
+++ test/test.go
@@ -10,7 +10,8 @@ import (
 const (
 	name = "Eric"
 )
-func main()  {
+
+func main() {
 	//c := [...]int{4, 5, 6}
 	//d := `hello
 	//world`
@@ -19,21 +20,22 @@ func main()  {
 	//if err != nil  {
 	//	fmt.Println(err)
 	//}
+
 	const (
 		x = iota // x == 0
 		y = iota // y == 1
 		z = iota // z == 2
 		w
 	)
-	fmt.Println( x, y, z,w)
-	var arr [10]int  // 声明了一个int类型的数组
-	arr[0] = 42      // 数组下标是从0开始的
-	arr[1] = 13      // 赋值操作
+	fmt.Println(x, y, z, w)
+	var arr [10]int // 声明了一个int类型的数组
+	arr[0] = 42     // 数组下标是从0开始的
+	arr[1] = 13     // 赋值操作
 	c := [...]int{4, 5, 6}
-	fmt.Printf("The first element is %d\n", arr[0])  // 获取数据，返回42
+	fmt.Printf("The first element is %d\n", arr[0]) // 获取数据，返回42
 	fmt.Printf("The last element is %d\n", arr[9])
 	fmt.Println(c)
-	slice := []byte {'a', 'b', 'c', 'd'}
+	slice := []byte{'a', 'b', 'c', 'd'}
 	fmt.Println(len(slice))
 
 	//假设用户名 abc，密码 123456
@@ -44,7 +46,7 @@ func main()  {
 	io.WriteString(h, pwd)
 
 	// pwmd5 等于 e10adc3949ba59abbe56e057f20f883e
-	pwmd5 :=fmt.Sprintf("%x", h.Sum(nil))
+	pwmd5 := fmt.Sprintf("%x", h.Sum(nil))
 
 	// 指定两个 salt： salt1 = @#$%   salt2 = ^&*()
 	salt1 := "@#$%"
@@ -56,7 +58,20 @@ func main()  {
 	io.WriteString(h, salt2)
 	io.WriteString(h, pwmd5)
 
-	last :=fmt.Sprintf("%x", h.Sum(nil))
+	last := fmt.Sprintf("%x", h.Sum(nil))
 	fmt.Println(last)
+	const (
+		_  = iota
+		KB = 1 << (10 * iota)
+		MB = 1 << (10 * iota)
+		GB = 1 << (10 * iota)
+		TB = 1 << (10 * iota)
+		PB = 1 << (10 * iota)
+	)
+	fmt.Println(KB)
+	fmt.Println(MB)
+	fmt.Println(GB)
+	fmt.Println(TB)
+	fmt.Println(PB)
 
 }
