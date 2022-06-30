570cd04a429902c1fb408c0896e6fe09fe319846
eric <799050481@qq.com>
2022-06-30 22:23:37 +0800

    test
    Signed-off-by: eric <799050481@qq.com>

--- web/gin.go
+++ web/gin.go
@@ -1,20 +1,43 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"html/template"
+	"net/http"
+	"time"
+)
+func formatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d/%02d/%02d", year, month, day)
+}
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
 
 	r.POST("/create",func(c *gin.Context){
-		c.JSON(200,gin.H{
+		c.JSON(http.StatusOK,gin.H{
 			"message":"OK",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	r.SetFuncMap(template.FuncMap{
+		"formatAsDate": formatAsDate,
+	})
+	//r.Delims("{[{", "}]}") //自定义界定符
+	r.LoadHTMLGlob("templates/*")
+	//r.LoadHTMLFiles("templates/index.tmpl") //等价于
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK,"index.tmpl", gin.H{
+			"title":"Eric",
+			"Unix":time.Now(),
+		})
+	})
+	r.Run(":8888") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
--- web/sub.go
+++ web/sub.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	for i := 1; i <= 9; i++ {
 		for j := 1; j <= i; j++ {
-			fmt.Printf("%d*%d:=%d ", j, i, j*i)
+			fmt.Printf("%d*%d=%2d ", j, i, j*i)
 		}
 		fmt.Println()
 	}
