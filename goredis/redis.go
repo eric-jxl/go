66ad2f19a6a8d2d2419fd37085871a7c14a2809a
eric <799050481@qq.com>
2022-12-03 22:30:02 +0800

    add redis lock
    Signed-off-by: eric <799050481@qq.com>

---
+++ goredis/redis.go
@@ -0,0 +1,50 @@
+package goredis
+
+import (
+	"context"
+	"log"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var (
+	rdb *redis.Client
+	ctx = context.Background()
+	mutex sync.Mutex
+)
+
+
+//InitRedis 初始化Redis连接
+func InitRedis() *redis.Client{
+	r := NewRedisStd()
+	val := []string{r.Ip, strconv.Itoa(r.Port)}
+	rdb = redis.NewClient(&redis.Options{
+		Addr:   strings.Join(val , ":"),
+		Password: r.Password, // no password set
+		DB:       0,  // use default DB
+	})
+	return rdb
+}
+
+// Lock
+func Lock(key string) bool {
+	mutex.Lock()
+	defer mutex.Unlock()
+	result, err := rdb.SetNX(ctx, key, 1, 10*time.Second).Result()
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return result
+}
+func UnLock(key string) int64 {
+	nums, err := rdb.Del(ctx, key).Result()
+	if err != nil {
+		log.Println(err.Error())
+		return 0
+	}
+	return nums
+}
---
+++ goredis/redis_test.go
@@ -0,0 +1,61 @@
+package goredis
+
+import "testing"
+
+func TestInitRedis(t *testing.T) {
+	r := InitRedis()
+	pong,err := r.Ping(ctx).Result()
+	if err != nil {
+		t.Fatalf("%v",err)
+	}
+	t.Log(pong)
+}
+
+func TestLock(t *testing.T) {
+	type args struct {
+		key string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Lock",
+			args{
+				"get",
+			},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Lock(tt.args.key); got != tt.want {
+				t.Errorf("Lock() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUnLock(t *testing.T) {
+	type args struct {
+		key string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{
+			"Unlock",
+			args{"get"},
+			1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := UnLock(tt.args.key); got != tt.want {
+				t.Errorf("UnLock() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
